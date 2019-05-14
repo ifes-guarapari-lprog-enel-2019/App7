c6f4a0de69ec4edf30339553b46fcbf1ddececc9
walberbeltrame <walber.beltrame@gmail.com>
2019-05-14 09:25:11 -0300

    Versão inicial

---
+++ App7/App7/Modelos/Curso.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App7.Modelos
+{
+    class Curso
+    {
+        // lista de disciplinas
+        private List<Disciplina> disciplinas;
+
+        public List<Disciplina> Disciplinas
+        {
+            get
+            {
+                return this.disciplinas;
+            }
+            set
+            {
+                this.disciplinas = value;
+            }
+        }
+
+        // atributo nome
+        private String nome;
+
+        public String Nome
+        {
+
+            get
+            {
+                return this.nome;
+            }
+
+        }
+
+        // construtor da classe
+        public Curso(String nome)
+        {
+            this.nome = nome;
+        }
+
+    }
+}
